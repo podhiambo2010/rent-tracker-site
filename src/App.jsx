16f08a94b502ff9d3589806518b7fffa5b758326
Peter Odhiambo <podhiambo2010@users.noreply.github.com>
2026-01-19 21:55:25 +0300

    modified app.jsx to improve user interface and fixed bugs in the authentication module.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import DashboardLayout from "./layout/DashboardLayout";
 import OverviewPage from "./dashboard/OverviewPage";
 import ArchivedPaymentsPage from "./dashboard/payments/archived/ArchivedPaymentsPage";
+import LeasesPage from "./leases/LeasesPage";
 
 export default function App() {
   return (
@@ -11,13 +12,11 @@ export default function App() {
       <Route element={<DashboardLayout />}>
         <Route path="/" element={<OverviewPage />} />
         <Route path="/archived-payments" element={<ArchivedPaymentsPage />} />
+        <Route path="/leases" element={<LeasesPage />} />
         {/* Future pages */}
         {/* <Route path="/payments" element={<PaymentsPage />} /> */}
-        {/* <Route path="/leases" element={<LeasesPage />} /> */}
         {/* <Route path="/bank-transactions/:id" element={<BankTransactionPage />} /> */}
       </Route>
     </Routes>
   );
 }
-
-<Route path="/leases" element={<LeasesPage />} />
