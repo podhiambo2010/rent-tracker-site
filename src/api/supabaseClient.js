5d354cfd88a1506129c2bd09668ebcf00c6192b7
Peter Odhiambo <podhiambo2010@users.noreply.github.com>
2026-03-26 18:41:54 +0300

    modified:src/api/supabaseClient.js

--- src/api/supabaseClient.js
+++ src/api/supabaseClient.js
@@ -3,8 +3,14 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+// Strong validation (both variables)
 if (!supabaseUrl) {
-  throw new Error("supabaseUrl is required.");
+  throw new Error("Missing VITE_SUPABASE_URL");
 }
 
+if (!supabaseKey) {
+  throw new Error("Missing VITE_SUPABASE_ANON_KEY");
+}
+
+// Create client
 export const supabase = createClient(supabaseUrl, supabaseKey);
