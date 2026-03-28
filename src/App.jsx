7b9682601c77827c17e07445a8c34c92ae172082
Peter Odhiambo <podhiambo2010@users.noreply.github.com>
2026-03-28 18:50:56 +0300

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import OverviewPage from "./dashboard/OverviewPage";
 import ArchivedPaymentsPage from "./dashboard/payments/archived/ArchivedPaymentsPage";
 import DashboardLayout from "./layout/DashboardLayout";
 import LeasesPage from "./leases/LeasesPage";
+import NewPayment from "./payments/NewPayment";
 
 export default function App() {
   return (
@@ -13,7 +14,7 @@ export default function App() {
         <Route path="/archived-payments" element={<ArchivedPaymentsPage />} />
         <Route path="/leases" element={<LeasesPage />} />
         {/* Future pages */}
-        {/* <Route path="/payments" element={<PaymentsPage />} /> */}
+        <Route path="/payments/new" element={<NewPayment />} />
         {/* <Route path="/bank-transactions/:id" element={<BankTransactionPage />} /> */}
       </Route>
     </Routes>
